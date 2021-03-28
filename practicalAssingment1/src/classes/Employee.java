30000cc42f581e5ad3335192b55973dcfef1aff9
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-03-28 15:42:40 -0300

    attributes in Employee class

--- practicalAssingment1/src/classes/Employee.java
+++ practicalAssingment1/src/classes/Employee.java
@@ -2,7 +2,15 @@ package classes;
 
 import interfaces.ICalculateSalary;
 
+import java.util.Date;
+
 public abstract class Employee extends Person implements ICalculateSalary {
-//comment to commit
+    private static int idCount;
+    private String employeeId;
+    private Date startDate;
+    private Date endDate;
+    private double salary;
+
+
 
 }
