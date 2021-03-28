8157cb74aeb3ccfebd0cc120f907908191d4f76c
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-03-27 21:40:53 -0300

    attributes and methods for SalesItem

--- practicalAssingment1/src/classes/Employee.java
+++ practicalAssingment1/src/classes/Employee.java
@@ -3,4 +3,6 @@ package classes;
 import interfaces.ICalculateSalary;
 
 public abstract class Employee extends Person implements ICalculateSalary {
+
+
 }
--- practicalAssingment1/src/classes/SalesItem.java
+++ practicalAssingment1/src/classes/SalesItem.java
@@ -1,10 +1,79 @@
 package classes;
 
 public class SalesItem {
-    private String salesId;
+    private static int codeCount;
+    private String itemCode;
+    private String itemId; //name
     private String description;
     private int quantity;
     private float unitPrice;
 
+    public SalesItem(String itemId, String description, int quantity, float unitPrice) {
+        this.itemCode =
+        this.itemId = itemId;
+        this.description = description;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
 
+
+    private static String addCodeCount() {
+        SalesItem.codeCount++;
+        return String.valueOf(SalesItem.codeCount);
+    }
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public float calculatePrice(){
+        return this.unitPrice*this.quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "SalesItem{" +
+                "itemCode='" + itemCode + '\'' +
+                ", itemId='" + itemId + '\'' +
+                ", description='" + description + '\'' +
+                ", quantity=" + quantity +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
 }
