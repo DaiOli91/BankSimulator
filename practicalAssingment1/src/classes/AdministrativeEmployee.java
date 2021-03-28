c763e6ea993c9b256f8e1b9aacdb3106bd23fead
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-03-28 16:13:20 -0300

    Attributes and methods for Emloyee and AdmEmp

--- practicalAssingment1/src/classes/AdministrativeEmployee.java
+++ practicalAssingment1/src/classes/AdministrativeEmployee.java
@@ -1,6 +1,19 @@
 package classes;
 
+import java.util.Date;
+
 public class AdministrativeEmployee extends Employee{
+    private double experience;
+
+    public AdministrativeEmployee() {
+        super();
+        this.experience = 0;
+    }
+    public AdministrativeEmployee(Date startDate, Date endDate, double salary, double experience) {
+        super(startDate, endDate, salary);
+        this.experience = experience;
+    }
+
     @Override
     public double annualSalary() {
         return 0;
--- practicalAssingment1/src/classes/Employee.java
+++ practicalAssingment1/src/classes/Employee.java
@@ -5,12 +5,73 @@ import interfaces.ICalculateSalary;
 import java.util.Date;
 
 public abstract class Employee extends Person implements ICalculateSalary {
-    private static int idCount;
+    private static int idCount = 1;
     private String employeeId;
     private Date startDate;
     private Date endDate;
-    private double salary;
+    private static double salary = 53600;
+
+    public Employee() {
+        this.employeeId = addIdCount();
+        this.startDate = new Date();
+        this.endDate = new Date();
+    }
+    public Employee(Date startDate, Date endDate, double salary) {
+        this.employeeId = addIdCount();
+        this.startDate = startDate;
+        this.endDate = endDate;
+        setSalary(salary);
+    }
+
+    public Employee(String idNumber, String firstName, String lastName, String nationality, Date birthDate, Date startDate, Date endDate, double salary) {
+        super(idNumber, firstName, lastName, nationality, birthDate);
+        addIdCount();
+        this.startDate = startDate;
+        this.endDate = endDate;
+        setSalary(salary);
+    }
 
 
+    public String getEmployeeId() {
+        return employeeId;
+    }
 
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    private static void setSalary(double newSalary) {
+        salary = newSalary;
+    }
+
+    private static String addIdCount(){
+        idCount++;
+        return String.valueOf(idCount);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId='" + employeeId + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", salary=" + salary +
+                '}';
+    }
 }
