c6cda9c7c1bc9f7f4efd5da59999192a9b3a6f06
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-03 09:48:53 -0300

    modifications to Client class

--- practicalAssingment1/src/classes/Client.java
+++ practicalAssingment1/src/classes/Client.java
@@ -1,29 +1,39 @@
 package classes;
 
-import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Client extends Person{
-    private static int clientCount = 0;
+    private static int clientCounter = 0;
     private String clientId;
-    private ArrayList<String> accountsIds; //the client may have several accounts
+    private static int length = 10;
+    private HashSet<String> accountsIds; //the client may have several accounts, but I only need their Ids
+
 
     public Client() {
-        this.clientId = addClientCount();
-        this.accountsIds= new ArrayList<>();
+        addClientCount();
+        generateClientId();
+        this.accountsIds= new HashSet<>();
     }
-    public Client(String clientId, String idAccount) {
-        this.clientId = clientId;
-        this.accountsIds= new ArrayList<>();
+
+    public Client(String idAccount) {
+        addClientCount();
+        generateClientId();
+        this.accountsIds= new HashSet<>();
         this.accountsIds.add(idAccount);
     }
 
-    private static int getClientCount() {
-        return clientCount;
+    public Client(String idNumber, String firstName, String lastName, String nationality, Date birthDate) {
+        super(idNumber, firstName, lastName, nationality, birthDate);
     }
 
-    private static String addClientCount() {
-        clientCount++;
-        return String.valueOf(clientCount);
+    private static int getClientCounter() {
+        return clientCounter;
+    }
+
+    private static void addClientCount() {
+        clientCounter++;
     }
 
 
@@ -34,18 +44,17 @@ public class Client extends Person{
     public String getIdAccount() {
         return idAccount;
     }
-
-    public String generarString(int length) {
-        while(this.nroSocio.length() < length) {
-            int aux_nroSocio = (char)((int)(Math.random() * 57.0D + 48.0D));
-            if (aux_nroSocio <= '9' && aux_nroSocio >= '0') {
-                this.nroSocio = this.nroSocio + (char)aux_nroSocio;
-            }
-        }
-
-        return this.nroSocio;
-    }
 */
+   private void generateClientId() {
+        String aux_clientId;
+        StringBuilder sb = new StringBuilder();
+        while(this.clientId.length() < Client.length) {
+            aux_clientId = String.valueOf(Math.random() * 57.0D + 48.0D);
+            sb.append(aux_clientId);
+        }
+        this.clientId = sb.toString();
+    }
+
     public void addIdAccount(String idAccount) {
         this.accountsIds.add(idAccount);
     }
