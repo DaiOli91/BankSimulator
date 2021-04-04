d504ed61159818ddc0231e3e07c174806e780815
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-04 20:57:07 -0300

    inner switch started... Cash Machine Simulator idea included

--- practicalAssingment1/src/app/BankSystem.java
+++ practicalAssingment1/src/app/BankSystem.java
@@ -8,39 +8,69 @@ public class BankSystem {
     private Bank LocalBank;
     private boolean exit = false;
     private int option = 0;
-    private int clientOption = 0;
+    private int innerOption = 0; //additional variable just in case
 
 
     public BankSystem() {
         this.LocalBank = new Bank();
     }
 
-    public void Menu (Scanner scan){
+    public void Menu(Scanner scan) {
         System.out.println("Welcome to LocalBank, please select an option to start operating");
 
-        while(!this.exit) {
+        while (!this.exit) {
             System.out.println("\n\tHow can we help you today?");
             System.out.println("1.- Operate with Clients");
             System.out.println("2.- Operate with Employees");
             System.out.println("3.- Operate with Accounts");
+            System.out.println("4.- Cash Machine Simulator");
             System.out.println("9.- To quit");
             System.out.println("Your choice: ");
             this.option = scan.nextInt();
-            switch(this.option) {
+            switch (this.option) {
                 case 1:
                     System.out.println("\nOption 1.- CLIENTS\n");
                     System.out.println("\nWhat would you like to do?\n");
+                    System.out.println("1.- See Clients List");
+                    System.out.println("2.- Look for a Client");
+                    System.out.println("3.- Add a new CLient");
+                    System.out.println("4.- Modify a Client");
+                    System.out.println("Your choice: ");
+                    this.option = scan.nextInt();
+                    switch (this.option) {
+                        case 1:
+                            System.out.println("\nOption 1.- CLIENTS\n");
+                            System.out.println("\nWhat would you like to do?\n");
 
 
-                  //  System.out.println(listadoEmpleados.mostrarTodos());
+                            //  System.out.println(listadoEmpleados.mostrarTodos());
+                            break;
+                        case 2:
+                            System.out.println("\nOption 2.- EMPLOYEES\n");
+                            //   System.out.println(listadoEmpleados.mostrarTodosDatosBasicos());
+                            break;
+                        case 3:
+                            System.out.println("\nOption 3.- ACCOUNTS\n");
+                            //   System.out.println(listadoEmpleados.getSueldoMayor().getDatosBasicos());
+                            break;
+                        case 9:
+                            System.out.println("\nOption 9.- Thank you for operating with us.\n");
+                            this.exit = true;
+                            break;
+                        default:
+                            System.out.println("\nI am afraid this option is not available.");
+                    }
+
+
+                    //  System.out.println(listadoEmpleados.mostrarTodos());
                     break;
                 case 2:
                     System.out.println("\nOption 2.- EMPLOYEES\n");
-                 //   System.out.println(listadoEmpleados.mostrarTodosDatosBasicos());
+                    //   System.out.println(listadoEmpleados.mostrarTodosDatosBasicos());
                     break;
                 case 3:
                     System.out.println("\nOption 3.- ACCOUNTS\n");
-                 //   System.out.println(listadoEmpleados.getSueldoMayor().getDatosBasicos());
+                    //   System.out.println(listadoEmpleados.getSueldoMayor().getDatosBasicos());
                     break;
                 case 9:
                     System.out.println("\nOption 9.- Thank you for operating with us.\n");
@@ -53,5 +83,5 @@ public class BankSystem {
         System.out.println("Have a nice day!");
     }
 
-    }
+}
 
