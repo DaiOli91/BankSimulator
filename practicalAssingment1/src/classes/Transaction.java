0c55436115c0fa69bfb98aa296432fa1ed8aedea
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-04 20:57:50 -0300

    class Transaction created - needed to have a list of operations within the Account class

---
+++ practicalAssingment1/src/classes/Transaction.java
@@ -0,0 +1,13 @@
+package classes;
+
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private LocalDateTime date;
+    // private String transactionId; // Idk if this is necessary...
+    private String description;
+    private double amount;
+
+
+}
