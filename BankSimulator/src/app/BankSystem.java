22f843aba7109d25d307189f0448d116eb9c3b42
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-04 21:11:03 -0300

    refactor project Name- modifications to BankSystem menu

--- BankSimulator/src/app/BankSystem.java
+++ BankSimulator/src/app/BankSystem.java
@@ -33,44 +33,51 @@ public class BankSystem {
                     System.out.println("\nWhat would you like to do?\n");
                     System.out.println("1.- See Clients List");
                     System.out.println("2.- Look for a Client");
-                    System.out.println("3.- Add a new CLient");
+                    System.out.println("3.- Add a new Client");
                     System.out.println("4.- Modify a Client");
                     System.out.println("Your choice: ");
                     this.option = scan.nextInt();
                     switch (this.option) {
                         case 1:
-                            System.out.println("\nOption 1.- CLIENTS\n");
-                            System.out.println("\nWhat would you like to do?\n");
+                            System.out.println("\nOption 1.- Show Client List\n");
+                            System.out.println(this.LocalBank.clientsToString());
 
-
-                            //  System.out.println(listadoEmpleados.mostrarTodos());
                             break;
                         case 2:
-                            System.out.println("\nOption 2.- EMPLOYEES\n");
-                            //   System.out.println(listadoEmpleados.mostrarTodosDatosBasicos());
+                            System.out.println("\nOption 2.- Look for a Client\n");
+                            System.out.println("\nPlease enter the Client Id Number\n");
+                            //TODO search client
+
                             break;
                         case 3:
-                            System.out.println("\nOption 3.- ACCOUNTS\n");
-                            //   System.out.println(listadoEmpleados.getSueldoMayor().getDatosBasicos());
+                            System.out.println("\nOption 3.- Add a new Client\n");
+                            //TODO add a client
+
                             break;
-                        case 9:
-                            System.out.println("\nOption 9.- Thank you for operating with us.\n");
-                            this.exit = true;
+                        case 4:
+                            System.out.println("\nOption 4.- Modify a Client\n");
+                            System.out.println("\nPlease enter the Client Id Number\n");
+                            //TODO modify client
+
                             break;
                         default:
                             System.out.println("\nI am afraid this option is not available.");
                     }
 
 
-                    //  System.out.println(listadoEmpleados.mostrarTodos());
+                    //  end of Case1 main Switch
                     break;
                 case 2:
                     System.out.println("\nOption 2.- EMPLOYEES\n");
-                    //   System.out.println(listadoEmpleados.mostrarTodosDatosBasicos());
+                    // TODO Employees option
                     break;
                 case 3:
                     System.out.println("\nOption 3.- ACCOUNTS\n");
-                    //   System.out.println(listadoEmpleados.getSueldoMayor().getDatosBasicos());
+                    //  TODO Accounts option
+                    break;
+                case 4:
+                    System.out.println("\nOption 4.- Cash Machine Simulator\n");
+                    // TODO Simulator switch
                     break;
                 case 9:
                     System.out.println("\nOption 9.- Thank you for operating with us.\n");
