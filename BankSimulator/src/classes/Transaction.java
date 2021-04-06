63a1cadae3b528cf74f02e826a5b606d176abed2
DaiOli91 <oliverautnfrmdpcampus@gmail.com>
2021-04-06 08:26:20 -0300

    Attributes and methods added

--- BankSimulator/src/classes/Transaction.java
+++ BankSimulator/src/classes/Transaction.java
@@ -2,12 +2,26 @@ package classes;
 
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 public class Transaction {
     private LocalDateTime date;
-    // private String transactionId; // Idk if this is necessary...
+    private String transactionId; // Idk if this is necessary, but it's usually present in bank registers
     private String description;
     private double amount;
 
+    ////////////////// CONSTRUCTORS
 
+    public Transaction() {
+    }
+
+    ////////////////// GETTERS
+    ////////////////// SETTERS
+    private void setTranstactionId() {
+        UUID aux_transactionId;
+        aux_transactionId = UUID.randomUUID();
+        this.transactionId = aux_transactionId.toString().substring(0, 9); //I just need a 10 digits long string
+    }
+    ////////////////// OTHERS
+    ////////////////// OVERRIDDEN
 }
